7610edbd39c432e22b2c6c449679346f9dd5095a
HARIHARAN D <hariharand2888@gmail.com>
2026-07-09 20:59:43 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (46.82%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int hIndex(vector<int>& cit) {
+
+        int n=cit.size();
+        int l=0, r=n-1;
+
+        while(l<=r){
+            int mid=(l+r)/2;
+            int cnt=n-mid;
+
+            if(cit[mid]==cnt)
+                return cnt;
+            else if (cit[mid]<cnt)
+                l=mid+1;
+            else
+                r=mid-1;
+        }
+
+        return n-l;
+        
+    }
+};
