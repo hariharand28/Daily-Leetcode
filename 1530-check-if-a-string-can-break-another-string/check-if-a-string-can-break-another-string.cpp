5637924a6c0e523f19a3544f22d5f00ad3a799c0
HARIHARAN D <hariharand2888@gmail.com>
2026-07-13 21:44:05 +0530

    Time: 31 ms (71.82%) | Memory: 14.5 MB (27.07%) - LeetSync

--- 1530-check-if-a-string-can-break-another-string/check-if-a-string-can-break-another-string.cpp
+++ 1530-check-if-a-string-can-break-another-string/check-if-a-string-can-break-another-string.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     bool checkIfCanBreak(string s1, string s2) {
-
-        sort(s1.begin(), s1.end());
-        sort(s2.begin(), s2.end());
-        int ans1 = 0, ans2 = 0;
-        int n = s1.size();
-        for (int i = 0; i < n; i++) {
-            if (s1[i] > s2[i])
-                ans1++;
-            else if (s2[i] > s1[i])
-                ans2++;
-            else {
-                ans1++;
-                ans2++;
+                sort(s1.begin(),s1.end());
+        sort(s2.begin(),s2.end());
+        bool ch1=true;
+        bool ch2=true;
+        for(int i=0;i<s1.length();i++){
+            if(s1[i]<s2[i]){
+                    ch1=false;
+            }
+            if(s1[i]>s2[i]){
+                ch2=false;
             }
         }
-        if (ans1 == n || ans2 == n)
-            return 1;
-        else
-            return 0;
+        return ch1||ch2;
         
     }
 };
