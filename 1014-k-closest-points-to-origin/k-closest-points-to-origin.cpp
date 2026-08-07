f3fd25128f5ce743cb525c7528618d434f3e880d
HARIHARAN D <hariharand2888@gmail.com>
2026-08-07 10:16:40 +0530

    Time: 21 ms (94.49%) | Memory: 61.6 MB (97.00%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    static bool cmp(vector<int>&a, vector<int>& b){
+        return (a[0]*a[0])+(a[1]*a[1])<(b[0]*b[0])+(b[1]*b[1]);}
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        sort(points.begin(), points.end(), cmp);
+        return vector<vector<int>>(points.begin(), points.begin()+k);
+    }};
