4e473f8a5390a03f5aaff61a3902472ad3fef7b9
HARIHARAN D <hariharand2888@gmail.com>
2026-08-11 12:58:45 +0530

    Time: 3 ms (30.82%) | Memory: 12.4 MB (9.48%) - LeetSync

---
+++ 93-restore-ip-addresses/restore-ip-addresses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+        vector<string> ans;
+
+    void back(string s, int in, int par, string ss){
+        if(par==4 && in==s.size()){
+            ss.pop_back();
+            ans.push_back(ss);
+        }
+
+        for(int i=1; i<=3 && i+in<=s.size(); i++){
+            string sub=s.substr(in, i);
+
+            if(sub.size()>1 && sub[0]=='0')
+                break;
+            
+            if(stoi(sub)>255)
+                break;
+            
+            back(s,in+i, par+1,ss+sub+'.');
+        }
+
+    }
+    vector<string> restoreIpAddresses(string s) {
+        if(s.size()>12) return {};
+        back(s,0,0,"");
+        return ans;
+    }
+};
